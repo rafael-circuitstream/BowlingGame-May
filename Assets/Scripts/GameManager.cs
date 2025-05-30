43b8bde1c27a40369f57a928895bcc9acdbff23f
rafael-circuitstream <rafael@circuitstream.ca>
2025-05-29 20:06:04 -0400

    Pin script was created

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        randomNumber = Random.Range(0, 101);
+        randomNumber = Random.Range(0, 2200);
         Instantiate(ballPrefab, transform.position, ballPrefab.transform.rotation);
     }
 
